041744110001d167d21152347495017eef1a5524
AULES\A5Alumno <ismagonzalezlopez@gmail.com>
2016-11-24 10:02:30 +0100

    Some changes

--- app/src/main/java/com/example/a5alumno/menu/MainActivity.java
+++ app/src/main/java/com/example/a5alumno/menu/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mMenuInflater = this.getMenuInflater();
         mMenuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.copyItem:
+                Toast.makeText(this,"Copy Item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cutItem:
+                Toast.makeText(this,"Cut Item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pasteItem:
+                Toast.makeText(this,"Paste Item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.searchItem:
+                Toast.makeText(this,"Search Item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.compareItem:
+                Toast.makeText(this,"Compare Item", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
